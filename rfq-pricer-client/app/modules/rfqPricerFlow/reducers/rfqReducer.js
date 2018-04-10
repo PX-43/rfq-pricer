b52d99be128a756f444415c53dbf763f1aa36e08
zo <zo@gmail.com>
2018-04-10 08:25:19 +0100

    cleanup

--- rfq-pricer-client/app/modules/rfqPricerFlow/reducers/rfqReducer.js
+++ rfq-pricer-client/app/modules/rfqPricerFlow/reducers/rfqReducer.js
@@ -1,93 +1,115 @@
 import  { types } from '../actions';
 
-export default (state = {}, action) => {
-  switch (action.type) {
-    case types.ON_RFQ_RECEIVED: {
-      //this destructuring removes allocations from rfq
-      //(this is not a spread operator here, but rest, as it is an assignment context)
-      const {allocations, ...rfq} = action.rfq;
-      return {
-        ...state,
-        [rfq.id]: {
-          ...rfq,
-          ccyNodes: [...allocations.map(ccyNode => {
-            const {valueDateNodes, ...ccy} = ccyNode;
+const isFwdPointsChanged = (ccyNodes, ccyNodeId, valueDateNodeId, fwdPoints) => {
+  const ccyNodeToUpdate = ccyNodes.find(item => item.id === ccyNodeId);
+  if(ccyNodeToUpdate){
+    const valueDateNodeToUpdate = ccyNodeToUpdate.valueDateNodes.find(item => item.id === valueDateNodeId);
+    return valueDateNodeToUpdate && valueDateNodeToUpdate.fwdPoints !== fwdPoints;
+  }
+};
+
+const isSpotChanged = (ccyNodes, ccyNodesId, spot) => {
+  const arrToUpdate = ccyNodes.find(item => item.id === ccyNodesId);
+  return arrToUpdate && arrToUpdate.spot !== spot;
+};
+
+const addNewRfq = (state, action) => {
+  //this destructuring removes allocations from rfq
+  //(this is not a spread operator here, but rest, as it is an assignment context)
+  const {allocations, ...rfq} = action.rfq;
+  return {
+    ...state,
+    [rfq.id]: {
+      ...rfq,
+      ccyNodes: [...allocations.map(ccyNode => {
+        const {valueDateNodes, ...ccy} = ccyNode;
+        return {
+          ...ccy,
+          rfqId:rfq.id,
+          valueDateNodes: [...valueDateNodes.map(valueDateNode => {
+            const {legs, ...valueDate} = valueDateNode;
             return {
-              ...ccy,
+              ...valueDate,
+              ccyPair: ccy.ccyPair,
+              precision: ccy.precision,
               rfqId:rfq.id,
-              valueDateNodes: [...valueDateNodes.map(valueDateNode => {
-                const {legs, ...valueDate} = valueDateNode;
-                return {
-                  ...valueDate,
-                  ccyPair: ccy.ccyPair,
-                  precision: ccy.precision,
-                  rfqId:rfq.id,
-                  ccyNodeId:ccy.id,
-                  legs: [...legs.map(leg => ({...leg}))]
-                }
-              })]
+              ccyNodeId:ccy.id,
+              legs: [...legs.map(leg => ({...leg}))]
             }
           })]
         }
-      };
+      })]
     }
+  };
+};
 
-    case types.ON_FWD_POINTS_CHANGED : {
-      const {ccyNodes, ...rfq} = state[action.rfqId];
+const updateFwdPoints = (state, action) => {
+  const {ccyNodes, ...rfq} = state[action.rfqId];
 
-      const ccyNodeToUpdate = ccyNodes.find(item => item.id === action.ccyNodeId);
-      if(ccyNodeToUpdate){
-        const valueDateNodeToUpdate = ccyNodeToUpdate.valueDateNodes.find(item => item.id === action.id);
-        if(valueDateNodeToUpdate && valueDateNodeToUpdate.fwdPoints === action.fwdPoints){
-          return state; //don't update state if there is no change
-        }
-      }
+  if(!isFwdPointsChanged(ccyNodes, action.ccyNodeId, action.id, action.fwdPoints)){
+    return state; //don't update state if there is no change
+  }
 
-      return {
-        ...state,
-        [rfq.id]: {
-          ...rfq,
-          ccyNodes: ccyNodes.map(ccyNode => {
-            if (ccyNode.id === action.ccyNodeId) {
-              const {valueDateNodes, ...ccy} = ccyNode;
-              return {
-                ...ccy,
-                valueDateNodes: valueDateNodes.map(valueDateNode => {
-                  if (valueDateNode.id === action.id) {
-                    return Object.assign({}, valueDateNode, {fwdPoints: action.fwdPoints})
-                  } else {
-                    return valueDateNode;
-                  }
-                })
+  return {
+    ...state,
+    [rfq.id]: {
+      ...rfq,
+      ccyNodes: ccyNodes.map(ccyNode => {
+        if (ccyNode.id === action.ccyNodeId) {
+          const {valueDateNodes, ...ccy} = ccyNode;
+          return {
+            ...ccy,
+            valueDateNodes: valueDateNodes.map(valueDateNode => {
+              if (valueDateNode.id === action.id) {
+                return {...valueDateNode, ...{fwdPoints: action.fwdPoints}};
+              } else {
+                return valueDateNode;
               }
-            } else {
-              return ccyNode;
-            }
-          })
+            })
+          }
+        } else {
+          return ccyNode;
         }
-      };
+      })
     }
+  };
+};
 
-    case types.ON_SPOT_CHANGED : {
-      const {ccyNodes, ...rfq} = state[action.rfqId];
-      const arrToUpdate = ccyNodes.find(item => item.id === action.id);
-      if(arrToUpdate && arrToUpdate.spot === action.spot){
-        return state; //don't update state if there is no change
-      }
-      return {
-        ...state,
-        [rfq.id]: {
-          ...rfq,
-          ccyNodes: ccyNodes.map(ccyNode => {
-            if (ccyNode.id === action.id) {
-              return Object.assign({}, ccyNode, {spot: action.spot})
-            } else {
-              return ccyNode;
-            }
-          })
+const updateSpot = (state, action) =>{
+  const {ccyNodes, ...rfq} = state[action.rfqId];
+
+  if(!isSpotChanged(ccyNodes, action.id, action.spot)){
+    return state; //don't update state if there is no change
+  }
+
+  return {
+    ...state,
+    [rfq.id]: {
+      ...rfq,
+      ccyNodes: ccyNodes.map(ccyNode => {
+        if (ccyNode.id === action.id) {
+          return {...ccyNode, ...{spot: action.spot}};
+        } else {
+          return ccyNode;
         }
-      };
+      })
     }
+  };
+};
+
+export default (state = {}, action) => {
+
+  switch (action.type) {
+
+    case types.ON_RFQ_RECEIVED:
+      return addNewRfq(state, action);
+
+    case types.ON_FWD_POINTS_CHANGED :
+      return updateFwdPoints(state, action);
+
+    case types.ON_SPOT_CHANGED :
+      return updateSpot(state, action);
+
     default:
       return state;
   }
