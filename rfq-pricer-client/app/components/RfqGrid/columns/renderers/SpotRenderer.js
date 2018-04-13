2022a0f0d8fe22128321af5d99fdef96b44b74e6
zo <zo@gmail.com>
2018-04-13 18:40:12 +0100

    adding parent cell ref to input cells

--- rfq-pricer-client/app/components/RfqGrid/columns/renderers/SpotRenderer.js
+++ rfq-pricer-client/app/components/RfqGrid/columns/renderers/SpotRenderer.js
@@ -1,43 +1,60 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import { rfqPricerFlowActions } from "../../../../modules/actions";
 import { priceUtils } from '../../../../utils';
 
-const update = (api, id, rfqId, spot, originalValue) => {
-  //todo: validate
-  const newSpot = Number.parseFloat(spot);
-  if(newSpot !== originalValue)
-    api.dispatchEvent(rfqPricerFlowActions.onSpotChanged(id, rfqId, newSpot));
-};
+class SpotRenderer extends PureComponent {
 
-const handleOnBlur = (evt, originalValue, id, rfqId, api) =>{
-  const spot = evt.target.value;
-  update(api, id, rfqId, spot, originalValue);
-};
-
-const handleKeyPress = (evt, originalValue, id, rfqId, api) => {
-  if(evt.key === 'Enter'){
-    const spot = evt.target.value;
-    update(api, id, rfqId, spot, originalValue);
+  constructor(props){
+    super(props);
+    this.editableElementContainer = React.createRef();
   }
-};
 
-export default props =>{
-  const {api, data: {spot, id, rfqId, precision}, node: {level}} = props;
+  update = (updatedValue) => {
+    const {api, data: {spot, id, rfqId}} = this.props;
+    //todo: validate
+    const newSpot = Number.parseFloat(updatedValue);
+    if(newSpot !== spot)
+      api.dispatchEvent(rfqPricerFlowActions.onSpotChanged(id, rfqId, newSpot));
+  };
 
-  if(level > 0)
-    return null;
+  handleOnBlur = (evt) => {
+    const spot = evt.target.value;
+    this.update(spot);
+  };
 
-  console.log('rendering SpotRenderer');
+  handleKeyPress = (evt) => {
+    if(evt.key === 'Enter'){
+      const spot = evt.target.value;
+      this.update(spot);
+    }
+  };
 
-  const formattedSpot = priceUtils.addTrailingZeros(spot, precision);
+  componentDidMount() {
+    const cell = this.editableElementContainer.current.parentElement;
+    console.log(cell);
+  }
 
-  const originalValue = spot;
-  return(
-    <div className='editable-cell-content'>
-      <input type='text' defaultValue={formattedSpot}
-             onBlur={evt => handleOnBlur(evt, originalValue, id, rfqId, api)}
-             onKeyPress={evt => handleKeyPress(evt, originalValue, id, rfqId, api)} />
-    </div>
-  );
 
-};
+  render(){
+
+    const {data: {spot, precision}, node: {level}} = this.props;
+
+    if(level > 0)
+      return null;
+
+    console.log('rendering SpotRenderer');
+
+    const formattedSpot = priceUtils.addTrailingZeros(spot, precision);
+
+    return(
+      <div className='editable-cell-content' ref={this.editableElementContainer}>
+        <input type='text' defaultValue={formattedSpot}
+               onBlur={evt => this.handleOnBlur(evt)}
+               onKeyPress={evt => this.handleKeyPress(evt)} />
+      </div>
+    );
+  }
+
+}
+
+export default SpotRenderer;
